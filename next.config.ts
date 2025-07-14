ac9653934478931c302f9b6aa2b97ae9e0c9c315
Udayasooriyan Prabudeva <prabud0401@gmail.com>
2025-07-14 08:51:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.web.imagine.art',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
