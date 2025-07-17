83b0b02d08afb55e9bbaec861ae7a49462314afa
Udayasooriyan Prabudeva <prabud0401@gmail.com>
2025-07-17 18:10:55 +0000

    can you this in the head? <script async src="https://pagead2.googlesyndi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4363233136452065"
+     crossOrigin="anonymous"></script>
       </head>
       <body className="font-body antialiased bg-background text-foreground">
           <AuthProvider>
