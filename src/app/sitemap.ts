d2ec33f80f64b5ead3dfdf98fc50c54b539c7962
Udayasooriyan Prabudeva <prabud0401@gmail.com>
2025-07-20 10:20:16 +0000

    still i can see one   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL |

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://visionhub.pics';
+  const baseUrl = 'http://visionhub.pics';
 
   const marketingPages = [
     '/',
