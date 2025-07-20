656f0c45d2d2051f64ed291aeedab569e74a0e7a
Udayasooriyan Prabudeva <prabud0401@gmail.com>
2025-07-20 10:21:05 +0000

    or http://localhost:9002/ also base url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'http://visionhub.pics';
+  const baseUrl = 'http://localhost:9002';
 
   const marketingPages = [
     '/',
